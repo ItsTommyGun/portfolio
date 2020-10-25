54f5b9aed9d4159641fb9bbfeb1a2ab537667547
Thomas Fischer <thomas.fischer@odt.net>
2020-10-25 19:39:09 +0100

    fix problem with observer

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -3,16 +3,17 @@ import { useEffect } from 'react';
 export default function animateInView(refElement, animationStartFrom, customThreshold) {
     const animationDirection = animationStartFrom ? animationStartFrom : 'bottom';
     const threshold = customThreshold ? customThreshold : '.5'
-  
-    var observer = new IntersectionObserver(function(entries) {
-        if(entries[0].isIntersecting === true)
-        //console.log(refElement.current.querySelector('.opacity-0'))
-        for (let item of refElement.current.children) {
-            item.classList.add('fadeIn');
-        }
-    }, { threshold: [threshold] });
 
     useEffect(() => {
+  
+        var observer = new IntersectionObserver(function(entries) {
+            if(entries[0].isIntersecting === true)
+            //console.log(refElement.current.querySelector('.opacity-0'))
+            for (let item of refElement.current.children) {
+                item.classList.add('fadeIn');
+            }
+        }, { threshold: [threshold] });
+        
         observer.observe(refElement.current);
     }, [])
 
