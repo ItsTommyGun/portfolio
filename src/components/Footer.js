4d787af3d563120e57fff4690458ccb73ef01f38
Thomas Fischer <thomas.fischer@odt.net>
2020-12-03 10:25:08 +0100

    Minor updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby-plugin-modal-routing'
+import ExternalLinkIcon from '../../static/icons/external-link.svg'
 
 export default function Footer(){
 
@@ -8,7 +9,11 @@ export default function Footer(){
             <div className='lg:container m-auto flex justify-between px-10 xl:px-0'>
                 <div>&copy; 2020 Thomas Fischer</div>
                 <div>
-                    <Link to='imprint'>Impressum</Link>
+                    Fotos von <a href='https://www.evamariajanson.com/' target='_blank' className='text-orange-600'>
+                        Eva Maria Janson
+                        <ExternalLinkIcon className='fill-current w-3 h-3 inline-block ml-1 align-baseline' />
+                    </a>
+                    <Link to='imprint' className='ml-10 text-orange-600'>Impressum / Haftung</Link>
                 </div>
             </div>
         </div>
